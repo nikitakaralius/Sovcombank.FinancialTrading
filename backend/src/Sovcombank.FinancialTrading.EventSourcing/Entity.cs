ca275e722df87d071437fb791f1fd0833e6b6d0c
nikitakaralyus <nikitakaralyus@gmail.com>
2022-11-19 00:02:39 +0300

    Created Entity

--- backend/src/Sovcombank.FinancialTrading.EventSourcing/Entity.cs
+++ backend/src/Sovcombank.FinancialTrading.EventSourcing/Entity.cs
@@ -1,6 +1,21 @@
 namespace Sovcombank.FinancialTrading.EventSourcing;
 
-internal sealed class Entity
+public abstract class Entity : IInternalEventHandler
 {
-    
+    private readonly Action<IEvent> _applier = null!;
+
+    protected Entity(Action<IEvent> applier) =>
+        _applier = applier;
+
+    protected Entity() { }
+
+    protected abstract void When(IEvent eventHappened);
+
+    protected void Apply(IEvent @event)
+    {
+        When(eventHappened: @event);
+        _applier(@event);
+    }
+
+    void IInternalEventHandler.Handle(IEvent @event) => When(eventHappened: @event);
 }
