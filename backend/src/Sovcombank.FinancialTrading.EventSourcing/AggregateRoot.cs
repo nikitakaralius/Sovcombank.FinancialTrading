1dd3ebda1d28b473bdf85731ab9a47ef169bf79f
nikitakaralyus <nikitakaralyus@gmail.com>
2022-11-19 00:02:58 +0300

    Created AggregateRoot

--- backend/src/Sovcombank.FinancialTrading.EventSourcing/AggregateRoot.cs
+++ backend/src/Sovcombank.FinancialTrading.EventSourcing/AggregateRoot.cs
@@ -1,6 +1,40 @@
 namespace Sovcombank.FinancialTrading.EventSourcing;
 
-internal sealed class AggregateRoot
+public abstract class AggregateRoot<TId> : IInternalEventHandler
 {
-    
+    private readonly List<IEvent> _changes = new();
+
+    public TId Id { get; protected set; }
+
+    public int Version { get; private set; } = -1;
+
+    public IEnumerable<IEvent> Changes() => _changes;
+
+    public void ClearChanges() => _changes.Clear();
+
+    public void Load(IEnumerable<IEvent> history)
+    {
+        foreach (var e in history)
+        {
+            When(e);
+            Version++;
+        }
+    }
+
+    protected abstract void When(IEvent eventHappened);
+
+    protected abstract void EnsureValidState();
+
+    protected void Apply(IEvent @event)
+    {
+        When(eventHappened: @event);
+        EnsureValidState();
+        _changes.Add(@event);
+    }
+
+    protected void ApplyToEntity(IInternalEventHandler? entity, IEvent @event) =>
+        entity?.Handle(@event);
+
+    void IInternalEventHandler.Handle(IEvent @event) =>
+        When(eventHappened: @event);
 }
