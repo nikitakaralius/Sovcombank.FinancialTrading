813cb0088af5166b0de70dd08394de93407fcc45
nikitakaralyus <nikitakaralyus@gmail.com>
2022-11-19 21:30:08 +0300

    Removed redundant currency fields

--- backend/src/Sovcombank.FinancialTrading.Domain/Account/Currency.cs
+++ backend/src/Sovcombank.FinancialTrading.Domain/Account/Currency.cs
@@ -4,14 +4,6 @@ public sealed record Currency
 {
     public int NumericCode { get; init; }
 
-    public string Code { get; init; } = "";
-
-    public int Nominal { get; init; }
-
-    public string Name { get; init; } = "";
-
-    public decimal Value { get; init; }
-
     public bool InUse { get; init; } = true;
 
     public static Currency None => new() {InUse = false};
