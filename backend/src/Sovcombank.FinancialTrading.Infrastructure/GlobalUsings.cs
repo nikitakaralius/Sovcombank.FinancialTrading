c9548a47ad409a476334fea13af2403490c07b6e
nikitakaralyus <nikitakaralyus@gmail.com>
2022-11-20 01:06:32 +0300

    Added extra global usings

--- backend/src/Sovcombank.FinancialTrading.Infrastructure/GlobalUsings.cs
+++ backend/src/Sovcombank.FinancialTrading.Infrastructure/GlobalUsings.cs
@@ -1,1 +1,5 @@
 global using EventStore.ClientAPI;
+global using Sovcombank.FinancialTrading.Domain.UserProfile;
+global using Sovcombank.FinancialTrading.Domain.UserProfile.ValueObjects;
+global using Sovcombank.FinancialTrading.Infrastructure.Serialization;
+global using Sovcombank.FinancialTrading.Infrastructure.Services;
