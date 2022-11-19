5d1f3a912f312f62c25c4149d60fa56af137d0a2
nikitakaralyus <nikitakaralyus@gmail.com>
2022-11-20 00:04:48 +0300

    Made Currency External Initialization with the CurrencyLookup service

--- backend/src/Sovcombank.FinancialTrading.Domain/Account/Currency.cs
+++ backend/src/Sovcombank.FinancialTrading.Domain/Account/Currency.cs
@@ -2,9 +2,23 @@ namespace Sovcombank.FinancialTrading.Domain.Account;
 
 public sealed record Currency
 {
-    public int NumericCode { get; init; }
+    internal Currency() { }
 
-    public bool InUse { get; init; } = true;
+    public int NumericCode { get; internal init; }
+
+    public bool InUse { get; private init; } = true;
+
+    public static Currency FromNumericCode(int numericCode, ICurrencyLookup currencyLookup)
+    {
+        var currency = currencyLookup.FindCurrency(numericCode);
+
+        if (currency.InUse == false)
+        {
+            throw new InvalidOperationException("Cannot create currency account with the currency that not in use");
+        }
+
+        return new Currency {NumericCode = numericCode};
+    }
 
     public static Currency None => new() {InUse = false};
 
