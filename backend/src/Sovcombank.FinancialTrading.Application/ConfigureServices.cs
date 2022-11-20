70394b2ac71289c9c8921092ee7ee22822aa7e40
nikitakaralyus <nikitakaralyus@gmail.com>
2022-11-20 05:46:29 +0300

    Registered Application Services

---
+++ backend/src/Sovcombank.FinancialTrading.Application/ConfigureServices.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Sovcombank.FinancialTrading.Application.Behaviors;
+
+namespace Sovcombank.FinancialTrading.Application;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
+
+        return services;
+    }
+}
